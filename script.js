5ab5fdaa97e15723fb3dd3cf5abea22c6aabe68e
Amanda Oliveira <62100844+oliveiraramanda@users.noreply.github.com>
2021-01-06 08:47:05 -0300

    Add files via upload
    Versão 1 finalizada

---
+++ script.js
@@ -0,0 +1,70 @@
+const holes = document.querySelectorAll('.hole');
+const scoreBoard = document.querySelector('.score');
+const moles = document.querySelectorAll('.mole');
+const countdownBoard = document.querySelector('.countdown');
+const startButton = document.querySelector('.startButton');
+
+
+let lastHole;
+let timeUp = false;
+let timeLimit = 20000;
+let score = 0;
+let countdown;
+
+function pickRandomHole (holes) {
+    const randomHole = Math.floor(Math.random() * holes.length);
+    const hole = holes[randomHole];
+    if (hole === lastHole){
+        return pickRandomHole(holes);
+    }
+
+    lastHole = hole;
+    return hole;
+}
+
+function popOut(){  
+    const time = Math.random() * 1300 + 400;
+    const hole = pickRandomHole(holes);
+    hole.classList.add('up');
+    setTimeout(function(){
+        hole.classList.remove('up');
+        if (!timeUp) popOut();
+    }, time);
+}
+
+function startGame(){
+    countdown = timeLimit/1000;
+    scoreBoard.textContent = 0;
+    scoreBoard.style.display = 'block';
+    countdownBoard.textContent = countdown;
+    timeUp = false;
+    score = 0;
+    popOut();
+    setTimeout(function(){
+        timeUp = true;
+    }, timeLimit)
+
+    let startCountdown = setInterval(function(){
+        countdown -=1;
+        countdownBoard.textContent = countdown;
+        if (countdown < 0){
+            countdown = 0;
+            clearInterval(startCountdown);
+            countdownBoard.textContent = 'Times Up! Thanks for see my ghost game, play again and make me happy'
+        }
+    }, 1000)
+}
+startButton.addEventListener('click', startGame);
+
+function whack (e){
+    score++;
+    this.style.backgroundImage = 'url(ghost2.png)';
+    this.style.pointerEvents = 'none';
+    setTimeout(() =>{
+        this.style.backgroundImage = 'url(ghost1.png)'
+        this.style.pointerEvents = 'all';
+    }, 800);
+    scoreBoard.textContent = score;
+
+}
+moles.forEach(mole => mole.addEventListener('click', whack));
